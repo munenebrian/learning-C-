c7c27bc472c2960488d198b5498d0976dadf7467
munenebrian <njerubrianmunene@gmail.com>
2022-08-11 14:46:24 +0300

    using string variables

--- programme.cs
+++ programme.cs
@@ -11,7 +11,11 @@ namespace Brian
             Console.WriteLine(" /   |")
             Console.WriteLine("/____|")
 
-            Console.WriteLine("yaaani!")
+            
+            Console.WriteLine("There once was a man named John")
+            Console.WriteLine("He was 35 years old")
+            Console.WriteLine("He really liked the name John")
+            Console.WriteLine("But didn't like being 70")
 
             Console.ReadLine();
         }
