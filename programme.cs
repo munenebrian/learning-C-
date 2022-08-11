a4d83b4b74c318670afa755d4e69a93a481123f6
munenebrian <njerubrianmunene@gmail.com>
2022-08-11 16:09:52 +0300

    simple input, and calculator

--- programme.cs
+++ programme.cs
@@ -10,12 +10,24 @@ namespace Brian
             Console.WriteLine("  /  |")
             Console.WriteLine(" /   |")
             Console.WriteLine("/____|")
-
+            string characterName = "John";
+            int characterAge = 35;
             
-            Console.WriteLine("There once was a man named John")
-            Console.WriteLine("He was 35 years old")
-            Console.WriteLine("He really liked the name John")
-            Console.WriteLine("But didn't like being 70")
+            Console.WriteLine("There once was a man named " + characterName);
+            Console.WriteLine("He was" + characterAge + "years old");
+            Console.WriteLine("He really liked the name" + characterName);
+            Console.WriteLine("But didn't like being" + characterAge);
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello" + name);
+
+            Console.Write("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num1 + num2);
+
 
             Console.ReadLine();
         }
